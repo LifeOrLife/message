11c60080f4785842ef92d07d9036d2e34ba4fe17
btea <2356281422@qq.com>
2022-02-07 14:41:31 +0800

    feat: adjust parameter

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ type Options = {
     text: string;
     duration?: number;
     style?: style;
+    devitation?: number;
 };
 type style = {
     [key: string]: string | number;
@@ -11,21 +12,23 @@ type style = {
 export default function LLmessage(params: msg | Options) {
     let msg: string,
         time: number = 3000,
-        style: style | undefined;
+        style: style | undefined,
+        dev: number = 30;
     if (typeof params === 'string') {
         msg = params;
     } else {
         msg = params.text;
         time = params.duration || 3000;
         style = params.style;
+        dev = params.devitation || 30;
     }
     if (!msg) {
         return;
     }
-    popupMsg(msg, time, style);
+    popupMsg(msg, time, dev, style);
 }
 
-function popupMsg(msg: string, time: number, style?: style) {
+function popupMsg(msg: string, time: number, dev: number, style?: style) {
     const _style: style = {
         padding: '5px 10px',
         'border-radius': '5px',
@@ -36,18 +39,17 @@ function popupMsg(msg: string, time: number, style?: style) {
         top: '0',
         left: '50%',
         transform: 'translateX(-50%)',
-        opacity: 0,
         transition: 'all .3s',
-        'word-break': 'break-all'
+        'word-break': 'break-all',
+        'font-size': '14px'
     };
     const box = document.createElement('div');
     box.innerText = msg;
     Object.assign(box.style, { ..._style, ...style });
     document.body.appendChild(box);
     setTimeout(() => {
-        box.style.top = '50px';
-        box.style.opacity = '1';
-    });
+        box.style.top = `${dev}px`;
+    })
     setTimeout(() => {
         box.style.top = '0';
         setTimeout(() => {
